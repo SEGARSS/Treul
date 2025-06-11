a0c453523bf9d26616a2916ab2db0599cf37af19
SEGARS <Vim_Branet@mail.ru>
2025-06-11 15:45:10 +0500

    работа дома

---
+++ Treul/main.cpp
@@ -0,0 +1,71 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <random>
+
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+using namespace sf;
+using namespace std;
+
+/*Сделай сначала один прямоугольник и выведи его*/
+//--------------------------------------------------------------------------
+RectangleShape getRectangleShape(float x, float y, int v, int b)
+{
+    RectangleShape rectangle;
+
+    rectangle.setSize(sf::Vector2f(v, b));
+    rectangle.setOutlineColor(sf::Color::Red);
+    rectangle.setOutlineThickness(5);
+    rectangle.setPosition({ x, y });
+
+    return rectangle;
+}
+//--------------------------------------------------------------------------
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(1, 500);
+    uniform_int_distribution<> distrib2(20, 100);
+    uniform_int_distribution<> distrib3(100, 400);
+
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML window");
+
+    vector<RectangleShape> rectangle;
+
+    //Вектор
+    vector<int> chisla;    
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        for (int i = 0; i < 1; i++)
+        {
+            chisla.push_back(distrib(gen));
+            rectangle.push_back(getRectangleShape(10, 100, 10, chisla[i]));
+        }
+
+        // Очистка окна.
+        window.clear();
+
+        for (int i = 0; i < rectangle.size(); i++)
+        {
+            window.draw(rectangle[i]);
+        }
+
+        //Обновить окно
+        window.display();
+    }
+    return 0;
+}
